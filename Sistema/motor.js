576ac85024500957f5cb4e7336313bd5a8e61367
luiz-guilhermee <80364703+luiz-guilhermee@users.noreply.github.com>
2022-05-14 21:44:58 -0300

    Add files via upload

---
+++ Sistema/motor.js
@@ -0,0 +1,16 @@
+
+document.getElementsByClassName("tablink")[0].click();
+
+function abrir(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].classList.remove("w3-light-grey");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.classList.add("w3-light-grey");
+}
