5567cc6f7ec3af90d77ef3d178a44840a2a7ccbc
Rafa-Pinheiro <rafaela.spinheiros@gmail.com>
2022-05-17 21:16:56 -0300

    Arq de apoio e mapa não respondendo

--- Sistema/teladomedaba3.php
+++ Sistema/teladomedaba3.php
@@ -5,9 +5,14 @@
 	    	<meta charset="utf-8">
     			
 
-	        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-					  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+			<link type="text/css" rel="stylesheet" href="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css" />
+			<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 				  <link rel="stylesheet" href="medaba3.css">
 
@@ -25,7 +30,17 @@
 	    					<li ><a id="aba3" href="teladomedaba3.php">Aba3</a></li>
 
 	    	</ul>
-
+		<div id="map"></div>
+		<form action="" class="form-group form" method="get">
+			<label for="uma" id="amb" class="col-sm-4 col-form-label">Nível da ambulância</label>
+			<select class="form-control col-sm-4" name="uma" id="uma">
+				<option value="" selected>Selecione</option>
+				<option value="A">Tipo A</option>
+				<option value="B">Tipo B</option>
+				<option value="C">Tipo C</option>
+				<option value="D">Tipo D</option>
+			</select>
+		</form>
 	  	<main>
 
 
