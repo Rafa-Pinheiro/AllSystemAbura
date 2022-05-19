98d9c45c3294cd456d092bf2c8883b61fb6af94f
Pedro-lima4336 <meucanal4336@gmail.com>
2022-05-18 22:00:55 -0300

    telas do aba1atendente ao aba2med atualizadas e identadas

--- Sistema/teladomedaba2.php
+++ Sistema/teladomedaba2.php
@@ -5,6 +5,7 @@
 <head class="fonte">
 	<meta charset="utf-8">
 
+	<script src="motor.js"></script>
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
