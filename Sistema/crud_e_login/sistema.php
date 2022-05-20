01f1c7904cba76384fa392821908098f8d2f2f9b
Raylla-Silva12 <raylla.l.silva@gmail.com>
2022-05-20 00:00:22 -0300

    atualizações do CRUD

--- Sistema/crud_e_login/sistema.php
+++ Sistema/crud_e_login/sistema.php
@@ -1,18 +1,18 @@
 <?php
-    session_start();
-    include_once('conexao.php');
-    /* print_r($_SESSION); */
-    if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
-        unset($_SESSION['email']);
-        unset($_SESSION['senha']);
-        header('Location: login.php');
-    }
-    $logado = $_SESSION['email'];
+session_start();
+include_once('conexao.php');
+/* print_r($_SESSION); */
+if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+  unset($_SESSION['email']);
+  unset($_SESSION['senha']);
+  header('Location: login.php');
+}
+$logado = $_SESSION['email'];
 
-    $sql = "SELECT * FROM tb_funcionario";
-    $result = $con->query($sql);
+$sql = "SELECT * FROM tb_funcionario";
+$result = $con->query($sql);
 
-    /* print_r($result); */
+/* print_r($result); */
 
 ?>
 
@@ -29,9 +29,42 @@
 
     <link rel="stylesheet" href="style.css">
 
-    <title>SISTEMA</title>
+    <center><title>SISTEMA</title></center>
 </head>
 <body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown link
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>    
+
     <h1>SISTEMA</h1>
     <a href="sair.php">Sair</a>
 
@@ -67,23 +100,27 @@
           <tbody>
             <?php
 
-              while ($user_data = mysqli_fetch_assoc($result)) {
-                  echo "<tr>";
-                  echo "<th>".$user_data['cd_rm_funcionario']."</th>";
-                  echo "<th>".$user_data['nm_funcionario']."</th>";
-                  echo "<th>".$user_data['ds_senha']."</th>";
-                  echo "<th>".$user_data['cd_cpf']."</th>";
-                  echo "<th>".$user_data['nr_cnh']."</th>";
-                  echo "<th>".$user_data['id_cargo']."</th>";
-                  echo "</tr>";
-              }
+while ($user_data = mysqli_fetch_assoc($result)) {
+  echo "<tr>";
+  echo "<th>" . $user_data['cd_rm_funcionario'] . "</th>";
+  echo "<th>" . $user_data['nm_funcionario'] . "</th>";
+  echo "<th>" . $user_data['ds_senha'] . "</th>";
+  echo "<th>" . $user_data['cd_cpf'] . "</th>";
+  echo "<th>" . $user_data['nr_cnh'] . "</th>";
+  echo "<th>" . $user_data['id_cargo'] . "</th>";
+  echo "</tr>";
+}
 
-            ?>
+?>
           </tbody>
 
         </table>
       </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 </body>
 </html>
