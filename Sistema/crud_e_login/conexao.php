6e93ed6588be2f1b6797a507fd4a1dd844daaa0e
Raylla-Silva12 <raylla.l.silva@gmail.com>
2022-05-23 22:39:14 -0300

    Organização de arquivos (sem ajuste de links)

--- Sistema/crud_e_login/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$con = new mysqli("localhost", "root", "usbw", "db_abura");
-	$con->set_charset("utf-8");
-
-	if ($con -> connect_errno) { /* Caso dê algum erro, o sistema avisa */
-		echo "Falha: erro número " . $con -> connect_errno;
-	}
-?>
