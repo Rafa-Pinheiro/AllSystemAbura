a3f674b36ba6a99a35f4e79d73019e6604927436
Ace871 <diego.souza286@etec.sp.gov.br>
2022-05-25 19:54:15 -0300

    Inserção das abas na pasta do projeto

---
+++ Sistema/abas_jquery/atendente.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/estiloAtendente.css">
+
+        <title>Tela Atendente</title>
+
+    </head>
+    <body>
+        
+        <div class="container">
+            <ul class="tab_navigation">
+                <li>ABA 1</li>
+                <li>ABA 2</li>
+            </ul>
+            <div class="tab_container_area">
+                <div class="tab_container">
+                    <form action="medico.php" method="POST">
+                        
+                        <center>
+                            <label>Campos de nome</label><br>
+                                <input type="text" name="nomeCompleto" placeholder="Nome completo"required><br>
+                                <input type="text" name="nomeSocorrido" placeholder="Nome do socorrido" required><br>
+                                <input type="number" name="faixaEtaria" placeholder="Digite a faixa etária do socorrido" required><br><br>
+                            
+                            <label>Possui Comorbidades</label><br>
+                                <input type="radio" name="coms" value="não" required>não.<br>
+                                <input type="radio" name="coms" value="sim" required>sim.<br><br>
+
+                            <label>Endereço</label><br>
+                                <input type="text" name="cidadeLocal" placeholder="Insira a cidade" required>
+                                <input type="text" name="bairroLocal" placeholder="Insira o bairro" required><br>
+                                <input type="text" name="ruaLocal" placeholder="Insira a rua" required>
+                                <input type="number" name="numeroLocal" placeholder="Informe o n°" required><br><br>
+
+                            <textarea name="descricao" placeholder="Insira a descrição" required></textarea>
+
+                            <input type="submit" value="Enviar">
+                        </center>
+
+                    </form>
+                </div>
+
+                <div class="tab_container">
+                    <main>
+                        <table>
+                            <tr>
+                                <td> 
+                                    <select name="ambulancia" id="select1">
+                                        <option value="volvo">Tipo de Ambulância</option>
+                                        <option value="A">A</option>
+                                        <option value="B">B</option>
+                                        <option value="C">C</option>
+                                    </select>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td> <select name="ambulancia2" id="select2">
+                                        <option value="volvo">Qtd.Ocorrência</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </table>
+
+                    </main>
+                </div>    
+            </div>
+        </div>
+
+
+        <script type="text/javascript" src="../js/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </body>
+
+</html>
--- Sistema/abas_jquery/medico.php
+++ Sistema/abas_jquery/medico.php
@@ -1,10 +1,22 @@
+<?php
+    $nome = $_POST['nomeCompleto'];
+    $nomeAcidentado = $_POST['nomeSocorrido'];
+    $faixaEtaria = $_POST['faixaEtaria'];
+    $comorbidades = $_POST['coms'];
+    $cidade = $_POST['cidadeLocal'];
+    $bairro = $_POST['bairroLocal'];
+    $rua = $_POST['ruaLocal'];
+    $numero = $_POST['numeroLocal'];
+    $descricao = $_POST['descricao'];
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/estiloAbas.css">
+        <link rel="stylesheet" type="text/css" href="css/estiloAtendente.css">
 
         <title>Tela do Médico</title>
 
@@ -25,18 +37,18 @@
                             <label>Campos de nome</label><br>
                                 <input type="text" name="nomeCompleto" placeholder="Nome completo" value="<?php echo $nome ?>" style="text-align: center;" readonly><br>
                                 <input type="text" name="nomeSocorrido" placeholder="Nome do socorrido" value="<?php echo $nomeAcidentado ?>" style="text-align: center;" readonly><br>
-                                <input type="number" name="faixaEtaria" placeholder="Digite a faixa etária do socorrido" value="<?php echo $faixaEtaria ?>" style="width: 205px; text-align: center;" required readonly><br><br>
+                                <input type="number" name="faixaEtaria" placeholder="Digite a faixa etária do socorrido" value="<?php echo $faixaEtaria ?>" style="width: 205px; text-align: center;" readonly><br><br>
                             
                             <label>Possui Comorbidades</label><br>
                                 <input type="text" name="comorbs" value="<?php if($comorbidades == "não"){echo $comorbidades;} else { echo $comorbidades;} ?>" style="text-align: center;" readonly><br><br>
 
                             <label>Endereço</label><br>
-                                <input type="text" name="cidadeLocal" placeholder="Insira a cidade" value="<?php echo $cidade ?>" style="margin-left: 10px; text-align: center;" required readonly>
-                                <input type="text" name="bairroLocal" placeholder="Insira o bairro" value="<?php echo $bairro ?>" style="margin-left: 10px; text-align: center;" required readonly><br>
-                                <input type="text" name="ruaLocal" placeholder="Insira a rua" value="<?php echo $rua ?>" style="margin-left: 10px; text-align: center;" required readonly>
-                                <input type="number" name="numeroLocal" placeholder="Informe o n°" value="<?php echo $numero ?>" style="margin-left: 10px; text-align: center;" required readonly><br><br>
+                                <input type="text" name="cidadeLocal" placeholder="Insira a cidade" value="<?php echo $cidade ?>" style="margin-left: 10px; text-align: center;" readonly>
+                                <input type="text" name="bairroLocal" placeholder="Insira o bairro" value="<?php echo $bairro ?>" style="margin-left: 10px; text-align: center;" readonly><br>
+                                <input type="text" name="ruaLocal" placeholder="Insira a rua" value="<?php echo $rua ?>" style="margin-left: 10px; text-align: center;" readonly>
+                                <input type="number" name="numeroLocal" placeholder="Informe o n°" value="<?php echo $numero ?>" style="margin-left: 10px; text-align: center;" readonly><br><br>
 
-                                <textarea name="descricao" style="text-align: center;" required></textarea>
+                                <textarea name="descricao" style="text-align: center;" readonly><?php echo htmlspecialchars($descricao); ?></textarea>
                         </center>
 
                      </form>
@@ -46,7 +58,7 @@
 
                     <main>
                         <label>*</label> <p>Descrição detalhada</p>
-                        <textarea id="descBasica" id="textarea" ></textarea>
+                        <textarea id="textarea" ></textarea>
 
                         <table>
                             <tr>
