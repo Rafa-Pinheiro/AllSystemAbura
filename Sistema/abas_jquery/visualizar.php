0aa9fa7f26c53b6f9e80e6cea5dfa87f919176a7
Raylla-Silva12 <raylla.l.silva@gmail.com>
2022-06-02 16:41:30 -0300

    ok

---
+++ Sistema/abas_jquery/visualizar.php
@@ -0,0 +1,18 @@
+<?php
+include '../conection/conexao.php';
+
+if (isset($_POST['updateid'])) {
+    $fun_id = $_POST['updateid'];
+
+    $sql = "SELECT * FROM tb_funcionario WHERE cd_funcionario=$fun_id";
+    $result = mysqli_query($mysqli, $sql);
+    $response = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $response = $row;
+    }
+    echo json_encode($response);
+} else {
+    $response['status']=200;
+    $response['messege']="Inválido";
+}
