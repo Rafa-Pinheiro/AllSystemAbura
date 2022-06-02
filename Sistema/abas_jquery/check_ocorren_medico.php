907b6b1e87624a9ab7415a7a55c690558a738dc6
Raylla-Silva12 <raylla.l.silva@gmail.com>
2022-06-02 14:04:29 -0300

    atualizações de estilo das abas

--- Sistema/abas_jquery/check_ocorren_medico.php
+++ Sistema/abas_jquery/check_ocorren_medico.php
@@ -13,7 +13,7 @@ include_once('../conection/conexao.php');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" >
     
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/estiloAtendente.css" >
+    <link rel="stylesheet" type="text/css" href="../css/style.css" >
 
     <title>Tela do Médico</title>
 </head>
@@ -24,50 +24,44 @@ include_once('../conection/conexao.php');
             <li>ABA 1</li>
             <li>ABA 2</li>
         </ul>
-        <div class="tab_container_area">
-            <div class="tab_container">
-                <form method="GET" style="margin-top: 20px">
-
-                    <!-- CÓDIGO ABA 1 -->
-                    <center>
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">ID Chamado</th>
-                                    <th scope="col">Nome Socorrido</th>
-                                    <th scope="col">Descrição Básica do Caso</th>
-                                    <th scope="col">Comorbidades</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                                $consulta = "SELECT * FROM tb_atendimento";
-                                $result = $mysqli->query($consulta);
-                                while ($row = $result->fetch_object()) {
-                                    echo "<tr>";
-                                    echo "<td> $row->cd_atendimento </td>";
-                                    echo "<td> $row->nm_socorrido </td>";
-                                    echo "<td> $row->ds_descricao_atendente </td>";
-                                    echo "<td> $row->st_comorbidade </td>";
-                                    echo "<td><a href='#'>Visualizar</a></td>";
-                                }
-                                ?>
-					        </tbody>
-                        </table>
-                    </center>
-                    
-                 </form>
-            </div>
-            <div class="tab_container">
-                <form method="POST" style="margin-top: 20px">
-                    <!-- CÓDIGO ABA DOIS -->
-                    <center>
-                        MAPA PARA VER AMBULÂNCIAS
-                    </center>
-                 </form>
-            </div>    
+        <div class="tab_container">
+            <form method="GET">
+                <!-- CÓDIGO ABA 1 -->
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID Chamado</th>
+                            <th scope="col">Nome Socorrido</th>
+                            <th scope="col">Descrição Básica do Caso</th>
+                            <th scope="col">Comorbidades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                        $consulta = "SELECT * FROM tb_atendimento";
+                        $result = $mysqli->query($consulta);
+                        while ($row = $result->fetch_object()) {
+                            echo "<tr>";
+                            echo "<td> $row->cd_atendimento </td>";
+                            echo "<td> $row->nm_socorrido </td>";
+                            echo "<td> $row->ds_descricao_atendente </td>";
+                            echo "<td> $row->st_comorbidade </td>";
+                            echo "<td><a href='#'>Visualizar</a></td>";
+                        }
+                        ?>
+				    </tbody>
+                </table>
+             </form>
         </div>
+        <div class="tab_container">
+            <form method="POST" style="margin-top: 20px">
+                <!-- CÓDIGO ABA DOIS -->
+                    MAPA PARA VER AMBULÂNCIAS
+             </form>
+        </div>    
     </div>
+
+
     <!-- SCRIPTS -->
     <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script> -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- Sistema/abas_jquery/ende_atendimento.php
+++ Sistema/abas_jquery/ende_atendimento.php
@@ -13,7 +13,7 @@ include_once('../conection/conexao.php');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous" >
     
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="css/estiloAtendente.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 
     <title>Tela Atendente</title>
 
